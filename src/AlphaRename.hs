ebb31df483c223bcce5eefec8e655bf253a9fc41
Thomas Li <guslithd@student.gu.se>
2020-04-15 03:42:28 +0200

    Began work on Alpha renaming module

---
+++ src/AlphaRename.hs
@@ -0,0 +1,109 @@
+{- | Module for running alpha-renaming of typechecked ASTs in order
+to make code generation easier. Each variable is replaced with
+a constant variable name suffixed with a unique counter.
+
+For example:
+
+int f() {
+  int x = 0;
+  int y = 2
+  {
+    int x = 5;
+  }
+  y = x;
+  return y
+}
+
+Converts to:
+
+int f() {
+  int v0 = 0;
+  int v1 = 2;
+  {
+    int v2 = 5;
+  }
+  int v3 = v0
+  return v3;
+}
+-}
+
+{-# LANGUAGE LambdaCase #-}
+
+module AlphaRename
+  ( alphaRename
+  ) where
+
+
+import           Control.Applicative ((<|>))
+
+import qualified Control.Monad.State as ST
+import qualified Data.Map.Strict as M
+
+import           Javalette.Abs
+
+newtype Original = Original Ident
+  deriving (Eq, Ord)
+
+-- | The environment maps the original names to their alpha-renamed names.
+
+type Context = M.Map Original Ident
+
+type Env = [Context]
+
+type Rename a = ST.State Env a
+
+runRename :: Rename a -> a
+runRename p = ST.evalState p emptyState
+  where
+    emptyState = []
+
+{- | Takes an annotated AST and returns a syntactically identical
+AST where all variable names are unique. That is, any variable assignments
+will appear only once per variable name.
+-}
+alphaRename :: Prog -> Prog
+alphaRename = runRename . rename
+
+rename :: Prog -> Rename Prog
+rename (Program topDefs) = Program <$> mapM renameDef topDefs
+
+renameDef :: TopDef -> Rename TopDef
+renameDef (FnDef typ id args (Block stmts)) = do
+  newArgs <- mapM renameArg args
+  newStmts <- mapM renameStmt stmts
+
+  return $ FnDef typ id newArgs (Block newStmts)
+
+  where
+    renameArg = undefined
+    renameStmt = undefined
+
+-- * Helper functions for manipulating the environment.
+
+-- | Adds an empty context to the top of the environment.
+pushCxt :: Rename ()
+pushCxt = ST.modify (M.empty :)
+
+{- | Removes the top context from the environment. Crashes if the
+environment is empty.
+-}
+popCxt :: Rename ()
+popCxt = ST.modify tail'
+  where
+    tail' :: [a] -> [a]
+    tail' = \case
+      []     -> error "popCxt: could not pop context from empty stack"
+      x : xs -> xs
+
+{- | Looks up the alpha-renamed version of an identifier, given the
+original name. Crashes if it cannot be found; the typechecking phase
+should make failure impossible.
+-}
+lookupVar :: Ident -> Rename Ident
+lookupVar id = ST.gets (find id) >>= \case
+  Just renamedVar -> return renamedVar
+  Nothing         -> error $ "lookupVar: could not find variable "
+                           ++ show id ++ " in environment"
+  where
+    find :: Ident -> Env -> Maybe Ident
+    find id = foldl (<|>) Nothing . map (M.lookup (Original id))
