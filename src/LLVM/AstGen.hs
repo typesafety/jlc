722576b81fad683c6ecbd6010e6e87b332dced12
Thomas Li <guslithd@student.gu.se>
2020-04-28 02:43:26 +0200

    AstGen: Got started

--- src/LLVM/AstGen.hs
+++ src/LLVM/AstGen.hs
@@ -8,9 +8,9 @@ other preprocessing/optimizations.
 -- TODO: Explicit export list
 module LLVM.AstGen where
 
-
-import Javalette.Abs
 import LLVM.ADT
+
+import qualified Javalette.Abs as J
 
 
 newtype ToADT a = ToADT a
@@ -29,9 +29,47 @@ instance Monad ToADT where
   return  = ToADT
   m >>= f = f . runToADT $ m
 
+--
+-- * "Boilerplate"-like content to include in .ll source files.
+--
 
-genLlvm :: Prog -> LLVM
+-- | Some external function definitions that should always be included.
+stdExtFunDefs :: [FunDecl]
+stdExtFunDefs =
+  [ FunDecl i32    (globalId "readInt")     []
+  , FunDecl Double (globalId "readDouble")  []
+  , FunDecl Void   (globalId "printInt")    [Arg i32 (localId "n")]
+  , FunDecl Void   (globalId "printDouble") [Arg Double (localId "d")]
+  , FunDecl
+      Void (globalId "printString") [Arg (TPointer $ iInt 8) (localId "s")]
+  ]
+
+--
+-- * LLVM Generation.
+--
+
+genLlvm :: J.Prog -> LLVM
 genLlvm = runToADT . genProg
 
-genProg :: Prog -> ToADT LLVM
-genProg = undefined
+genProg :: J.Prog -> ToADT LLVM
+genProg (J.Program topDefs) = undefined
+
+--
+-- * Helper function for LLVM ADT constructors.
+--
+
+-- | Create a global scope Ident (@var).
+globalId :: String -> Ident
+globalId = Ident Global
+
+-- | Create a local scope Ident (%var).
+localId :: String -> Ident
+localId = Ident Local
+
+-- | Shorthand for creating integers of size n bits.
+iInt :: Int -> Type
+iInt = TNBitInt
+
+-- | Shorthand for the very common i32 type.
+i32 :: Type
+i32 = iInt 32
